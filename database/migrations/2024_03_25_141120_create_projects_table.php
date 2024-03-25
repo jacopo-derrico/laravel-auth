5b0a1c8f289aed0196691b22d74ff6d500cb960e
jacopo-derrico <derricoj.design@gmail.com>
2024-03-25 16:24:07 +0100

    projects seed faker

--- database/migrations/2024_03_25_141120_create_projects_table.php
+++ database/migrations/2024_03_25_141120_create_projects_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100)->unique();
+            $table->string('slug');
+            $table->string('img');
+            $table->text('description');
+            $table->string('software');
             $table->timestamps();
         });
     }
